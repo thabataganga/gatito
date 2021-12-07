453297dfa6e47f42b1cff9cf9e246d74fb48c60f
thabataganga <ganga.thabata@gmail.com>
2021-12-07 05:08:47 -0300

    get find one

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -19,4 +19,21 @@ roteador.post('/', async(req, res) => {
     )
 })
 
+roteador.get('/:idFornecedor', async(req, res) => {
+    try {
+        const id = req.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+        await fornecedor.carregar()
+        res.send(
+            JSON.stringify(fornecedor)
+        )
+    } catch (erro) {
+        res.send(
+            JSON.stringify({
+                mensagem: erro.message
+            })
+        )
+    }
+})
+
 module.exports = roteador
