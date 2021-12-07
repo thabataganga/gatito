c48ac0f17d91dad750b20faa2a8235a4da3065c8
thabataganga <ganga.thabata@gmail.com>
2021-12-07 06:23:46 -0300

    status

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -5,6 +5,7 @@ const Fornecedor = require('./Fornecedor')
 // Declarar a primeira requisicao
 roteador.get('/', async(req, res) => {
     const resultados = await TabelaFornecedor.listar()
+    res.status(200)
     res.send(
         JSON.stringify(resultados)
     )
@@ -15,6 +16,7 @@ roteador.post('/', async(req, res) => {
         const dadosRecebidos = req.body
         const fornecedor = new Fornecedor(dadosRecebidos)
         await fornecedor.criar()
+        res.status(201)
         res.send(
             JSON.stringify(fornecedor)
         )
@@ -32,6 +34,7 @@ roteador.get('/:idFornecedor', async(req, res) => {
         const id = req.params.idFornecedor
         const fornecedor = new Fornecedor({ id: id })
         await fornecedor.carregar()
+        res.status(200)
         res.send(
             JSON.stringify(fornecedor)
         )
@@ -51,6 +54,7 @@ roteador.put('/:idFornecedor', async(req, res) => {
         const dados = Object.assign({}, dadosRecebidos, { id: id })
         const fornecedor = new Fornecedor(dados)
         await fornecedor.atualizar()
+        res.status(204)
         res.end()
     } catch (erro) {
         res.send(
@@ -67,6 +71,7 @@ roteador.delete('/:idFornecedor', async(req, res) => {
         const fornecedor = new Fornecedor({ id: id })
         await fornecedor.carregar()
         await fornecedor.remover()
+        res.status(204)
         res.end()
     } catch (erro) {
         res.send(
