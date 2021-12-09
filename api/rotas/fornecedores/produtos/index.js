db2919415a9e44a3f9d71ac68c0c123393283fa6
thabataganga <ganga.thabata@gmail.com>
2021-12-09 00:39:49 -0300

    midleware de verificação do fornecedor

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -3,7 +3,7 @@ const Tabela = require('./TabelaProduto')
 const Produto = require('./Produto')
 
 roteador.get('/', async(req, res) => {
-    const produtos = await Tabela.listar(req.params.idFornecedor)
+    const produtos = await Tabela.listar(req.fornecedor.id)
     res.send(
         JSON.stringify(produtos)
     )
@@ -11,7 +11,7 @@ roteador.get('/', async(req, res) => {
 
 roteador.post('/', async(req, res, proximo) => {
     try {
-        const idFornecedor = req.params.idFornecedor
+        const idFornecedor = req.fornecedor.id
         const corpo = req.body
         const dados = Object.assign({}, corpo, { fornecedor: idFornecedor })
         const produto = new Produto(dados)
@@ -26,7 +26,7 @@ roteador.post('/', async(req, res, proximo) => {
 roteador.delete('/:id', async(req, res) => {
     const dados = {
         id: req.params.id,
-        fornecedor: req.params.idFornecedor
+        fornecedor: req.fornecedor.id
     }
 
     const produto = new Produto(dados)
