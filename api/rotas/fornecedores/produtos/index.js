c61ae67075db3d116c62f661879d4bc01e2d4d3c
thabataganga <ganga.thabata@gmail.com>
2021-12-09 00:20:27 -0300

    apagar produto

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,11 +1,34 @@
 const roteador = require('express').Router({ mergeParams: true })
 const Tabela = require('./TabelaProduto')
+const Produto = require('./Produto')
 
 roteador.get('/', async(req, res) => {
     const produtos = await Tabela.listar(req.params.idFornecedor)
     res.send(
         JSON.stringify(produtos)
     )
+})
+
+roteador.post('/', async(req, res) => {
+    const idFornecedor = req.params.idFornecedor
+    const corpo = req.body
+    const dados = Object.assign({}, corpo, { fornecedor: idFornecedor })
+    const produto = new Produto(dados)
+    await produto.criar()
+    res.status(201)
+    res.send(produto)
+})
+
+roteador.delete('/:id', async(req, res) => {
+    const dados = {
+        id: req.params.id,
+        fornecedor: req.params.idFornecedor
+    }
+
+    const produto = new Produto(dados)
+    await produto.apagar()
+    res.status(204)
+    res.end()
 })
 
 module.exports = roteador
